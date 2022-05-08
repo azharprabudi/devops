6d9cee60ef869d02975a3d3a78a4aeea4ff7247e
azharprabudi <azharprabui@gmail.com>
2022-05-08 19:58:27 +0700

    update scripts to provision kubernetes locally

--- scripts/vagrant.mk
+++ scripts/vagrant.mk
@@ -1,4 +1,17 @@
-VAGRANT_PLUGINS:="vagrant-share"
+VAGRANT_BOXES:="generic/ubuntu2004"
+VAGRANT_PLUGINS:="vagrant-share vagrant-disksize"
+
+.PHONY: vagrant-local-init:
+vagrant-local-init:
+	apt-get install libarchive-tools
+	${MAKE} vagrant-download-boxes
+	${MAKE} vagrant-init-plugins
+
+.PHONY: vagrant-download-boxes
+vagrant-download-boxes:
+	@for box in ${VAGRANT_BOXES}; do \
+		vagrant box add $${box}; \
+	done
 
 .PHONY: vagrant-init-plugins
 vagrant-init-plugins:
@@ -8,11 +21,14 @@ vagrant-init-plugins:
 
 .PHONY: install-kubernetes-local
 install-kubernetes-local: vagrant-init-plugins
-	${MAKE} install-k8s-control-plane
+	@cd vagrant/kubernetes-local
+	vagrant up
+	vagrant provision
 
-.PHONY: install-kubernetes-control-plane
-install-kubernetes-control-plane:
-	@echo "test"
+.PHONY: destroy-kubernetes-local
+destroy-kubernetes-local:
+	@cd vagrant/kubernetes-local
+	vagrant destroy --force
 
 .PHONY: machines
 machines:
